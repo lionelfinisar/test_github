f8565e0acefd208096341cbc9c45b9c1dbc16476
Lionel Li <lionel.li@finisar.com>
2012-02-03 17:37:28 -0800

    first commit

---
+++ test_github/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGithub::Application.config.secret_token = '7598584a611219652b1e68d716eeaeec5946e05dab72c67add433a7fb9a07dc2afd8203adb00cf876b6cff73cbb8abe0702a5022478979b26e094f15c96cce5f'
